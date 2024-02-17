37b984637b983d2c2618720a7aba09a35f415ec7
Soso42 <sosojanashvili@gmail.com>
2024-02-17 11:52:00 +0400

    added task01 calculator

---
+++ Task01-calculator/main.js
@@ -0,0 +1,66 @@
+/** კალკულატორი
+ორ ოპერანდს შორის სხვადასხვა არითმეტიკული მოქმედებები (+, -, *, /, %, **)
+რიცხვების ვალიდირება
+შედეგის წარმატებით გამოტანა, სწორი ინპუტების შემთხვევაში javasc
+*/
+
+const VALID_OPERANDS = ['+', '-', '*', '/', '%', '**'];
+let num1;
+let num2;
+let operand;
+let result;
+
+
+while (true) {
+    num1 = getNumber();
+    operand = getOperand();
+    num2 = getNumber();
+
+    result = calculate(num1, num2, operand);
+    alert(result);
+
+    if (!userAgreesToContinue()) {
+        break;
+    }
+}
+
+function getNumber() {
+    while (true) {
+        let input = prompt('Please enter number ' );
+        if (!isNaN(input)) {
+            return Number(input);
+        }
+        alert('You must enter a number. Try again...');
+    }
+}
+
+function getOperand() {
+    while (true) {
+        let input = prompt('Please enter operand. valid operands are: ' + VALID_OPERANDS.toString());
+        if (VALID_OPERANDS.includes(input)) {
+            return input;
+        }
+        alert('Invalid operand. Try again...');
+    }
+}
+
+function calculate(num1, num2, operand) {
+    switch (operand) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            return num1 / num2;
+        case '%':
+            return num1 % num2;
+        case '**':
+            return num1 ** num2;
+    }
+}
+
+function userAgreesToContinue() {
+    return confirm("Would you like to perform another calculation? ");
+}
